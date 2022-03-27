1f1cf5e8573f17a0d9ae05d4c24b83b427d76bb7
hello2166 <smartmon@naver.com>
2022-03-28 00:10:45 +0900

    process login 2

--- app/src/routes/home/home-ctrl.js
+++ app/src/routes/home/home-ctrl.js
@@ -1,5 +1,9 @@
 
 
+const users = {
+    id: ['smartmon','hello2166','honkvvv'],
+    psword: ['1212','0202','2121'],
+}
 
 const output = {
     home: (req, res) => {
@@ -12,7 +16,21 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const   id = req.body.id,
+                psword = req.body.psword;
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                    msg: '로그인 성공',
+                })
+            }
+        }
+        return res.json({
+            success: false,
+            msg: '로그인 실퍠',
+        })
     },
 }
 

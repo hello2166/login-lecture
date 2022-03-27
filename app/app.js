4c0a241f83da3d18daae41ae59b8c1f4b6ed97d2
hello2166 <smartmon@naver.com>
2022-03-27 23:25:08 +0900

    process login

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const home = require('./src/routes/home');
@@ -7,10 +9,12 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json()); // app.use('/'.. 보다 위에 둬야 제대로 작동하는 듯
+//app.use(bodyParser.urlencoded({extended: true})); // url의 공백, 한글등 처리, 당장은 한글 전달 문제 없는 듯
+
 app.use('/', home);
 app.use(express.static(`${__dirname}/src/public`));
 
+
 module.exports = app;
-
-
 
